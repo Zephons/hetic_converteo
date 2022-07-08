c487b33efbf95c5e6bec0121c0718d3d865fabb6
Zhifeng LIANG <tzephons@gmail.com>
2022-07-08 09:44:39 +0200

    UPDATE(src): process, methods, aggregation

---
+++ src/aggregation.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+from process import preprocess, enrich
+
+
+postgresql_uri = os.environ["DATABASE_URL"]
+
+engine = create_engine(postgresql_uri.replace("postgres", "postgresql"))
+
+
+# On force le type de la colonne Zipcode en string.
+df = pd.read_excel("data/raw_datas_projet_M5D_Hetic.xlsx", header=[1], dtype={"Zipcode": str})
+df_preprocessed = preprocess(df)
+df_enriched = enrich(df_preprocessed)
+
+# Table pour pie chart
+df_pie = df_enriched.groupby(['Business Id', 'Sentiment'])['Sentiment'].count().reset_index(name="Count")
+df_pie.to_sql(name="pie_chart", con=engine, if_exists="replace")
+
